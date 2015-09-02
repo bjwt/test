2017801b8aec91148b2bba14df47f626915aba34
Tao Wu <bjwt@linux.vnet.ibm.com>
2015-09-02 16:33:02 +0800

    update urls.py to adapt django 1.6

--- eshop/order/urls.py
+++ eshop/order/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import *
 
 from models import *
 from views import buy,index,info
--- eshop/product/urls.py
+++ eshop/product/urls.py
@@ -1,5 +1,5 @@
 #coding=UTF-8
-from django.conf.urls.defaults import *
+from django.conf.urls import *
 
 from models import *
 from views import *
