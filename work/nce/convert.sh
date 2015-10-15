f42f9ad5cde628a7fbcbd439d4ff93e18e65d086
Tao Wu <bjwt@linux.vnet.ibm.com>
2015-10-15 14:00:01 +0800

    create the makeMobi.sh
    Signed-off-by: Tao Wu <bjwt@linux.vnet.ibm.com>

--- work/nce/convert.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-
-ebook-convert $1 $2 --authors 'Wu Tao' --cover nce4.jpg --share-not-sync --title "New Concept English" --mobi-toc-at-start --level1-toc '//h:h1' --level2-toc '//h:h2'
