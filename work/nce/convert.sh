23401af45ca0a4b60726b24919f1a3ae63de5c61
Tao Wu <bjwt@linux.vnet.ibm.com>
2015-10-15 10:51:03 +0800

    add bootstrap
    Signed-off-by: Tao Wu <bjwt@linux.vnet.ibm.com>

--- work/nce/convert.sh
+++ work/nce/convert.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-ebook-convert $1 $2 --authors 'Wu Tao' --cover nce4.jpg --share-not-sync --title "New Concept Englist" --mobi-toc-at-start --level1-toc '//h:h1' --level2-toc '//h:h2'
+ebook-convert $1 $2 --authors 'Wu Tao' --cover nce4.jpg --share-not-sync --title "New Concept English" --mobi-toc-at-start --level1-toc '//h:h1' --level2-toc '//h:h2'
