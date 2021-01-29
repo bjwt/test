a80eb412f63b0a9fcb5f365904a6ae2770f63209
Tao Wu <fedosu85@gmail.com>
2021-01-29 20:40:30 +0800

    add java first code

---
+++ work/java/codes/src/cn/it/Demo01.java
@@ -0,0 +1,14 @@
+package cn.it;
+
+import java.util.Date;
+
+public class Demo01 {
+    public static void main(String[] args) {
+        demo1();
+    }
+
+    public static void demo1(){
+        Date date = new Date();
+        System.out.println(date);
+    }
+}
